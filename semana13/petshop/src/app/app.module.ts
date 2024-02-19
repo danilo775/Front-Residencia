e34e0683b657ce743cd0ec36fb362ff632ba02c9
danilo775 <danilo.uba@hotmail.com>
2024-02-19 16:32:05 -0300

    Petshop

---
+++ semana13/petshop/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  { path: 'adicionaTicket', component: FormularioComponent },
+ 
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormularioComponent,
+    FormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ semana13/petshop/src/app/data-base.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { Ticket } from './ticket.model';
+import { map } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataBaseService implements OnInit {
+
+  loadedTickets: Ticket[] = [];
+
+  constructor(private http: HttpClient) { }
+
+
+
+  ngOnInit(): void {
+
+  } 
+
+  addTicket(ticketData: {  NomePassageiro: string, 
+                                numeroVoo: string, 
+                                dataPartida: string, 
+                                dataChegada: string, 
+                                aeroportoPartida: string, 
+                                aeroportoChegada: string }) {
+
+    this.http.post(
+      'https://petshop-2e94d-default-rtdb.firebaseio.com/posts.json',
+       ticketData)
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
+
+  getTickets() {
+    return this.http.get< {[key:string]: Ticket}>('https://petshop-2e94d-default-rtdb.firebaseio.com/posts.json',
+      {
+        params: new HttpParams().set('print', 'pretty')
+      }
+    )
+    .pipe(
+      map( (responseData) => {
+        const postArray:Ticket[] = [];
+        for (const key in responseData) {
+            if ((responseData).hasOwnProperty(key)){
+              postArray.push({...(responseData as any)[key], id: key});
+            }
+        }
+        return postArray;
+      }
+      )
+    );
+  }
+
+}
