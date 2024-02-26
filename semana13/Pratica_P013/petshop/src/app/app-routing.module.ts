0db9f3543096944d5777ccb40e3159800d367e6a
danilo775 <danilo.uba@hotmail.com>
2024-02-26 15:35:31 -0300

    atenticacao

--- semana13/Pratica_P013/petshop/src/app/app-routing.module.ts
+++ semana13/Pratica_P013/petshop/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './formulario/formulario.component';
 import { ContatoComponent } from './contato/contato.component';
 import { ListarPetComponent } from './listar-pet/listar-pet.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  { path: '*', component: AppComponent },
   { path: 'adicionarPet', component: FormularioComponent },
   { path: 'contato', component: ContatoComponent },
   { path: 'listarPet', component: ListarPetComponent },
