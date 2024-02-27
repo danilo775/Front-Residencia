3357d86ae58b2d5b370ce87fe353ef5cf7e1ddd8
danilo775 <danilo.uba@hotmail.com>
2024-02-27 10:32:11 -0300

    modulo

---
+++ semana14/crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.scss'
+})
+export class CoursesComponent implements OnInit {
+  
+  courses:  Course[] =[
+    {
+      _id: '1', name: 'Angular', category: 'frontend'
+    }
+  ];
+  displayedColumns: string[] = ['name', 'category'];
+  constructor() {
+    //this.courses =[];
+   }
+  ngOnInit() { }
+}
