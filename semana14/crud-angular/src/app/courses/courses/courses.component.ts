36507fcdd9326bb7370deb5de670f031e62b181f
danilo775 <danilo.uba@hotmail.com>
2024-02-28 16:47:30 -0300

    spinner

--- semana14/crud-angular/src/app/courses/courses/courses.component.ts
+++ semana14/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -9,14 +10,16 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
   //coursesService : CoursesService;
-  courses:  Course[] =[
   
-  ];
+  courses$: Observable<Course[]> ;
+
   displayedColumns: string[] = ['name', 'category'];
+
+
   constructor(private coursesService : CoursesService) {
   //  this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
-    //this.courses =[];
+  //this.courses =[];
+    this.courses$ = this.coursesService.list();
    }
   ngOnInit() { }
 }
