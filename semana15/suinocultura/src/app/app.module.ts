563625acc89c14d298a1f5ac23f82e1ba03bdf30
danilo775 <danilo.uba@hotmail.com>
2024-03-05 16:37:07 -0300

    autenticaçao

--- semana15/suinocultura/src/app/app.module.ts
+++ semana15/suinocultura/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -9,6 +10,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AutenticaInterceptor } from './interceptors/autentica.interceptor';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { AutenticaInterceptor } from './interceptors/autentica.interceptor';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AutenticaInterceptor, multi: true}],
   bootstrap: [AppComponent]
--- semana15/suinocultura/src/app/components/header/header.component.ts
+++ semana15/suinocultura/src/app/components/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AutenticaService } from '../../services/autentica.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
 
+export class HeaderComponent implements OnInit{
+    private inscricao!: Subscription;
+    estaAutenticado = false;
+  
+    constructor(private autenticaService: AutenticaService) { }
+  
+  
+    ngOnInit(): void {
+      this.inscricao = this.autenticaService.usuario.subscribe(
+        usuario => {
+          this.estaAutenticado = !usuario ? false : true;
+        });
+    }
+  
+    ngOnDestroy(): void {
+      this.inscricao.unsubscribe();
+    }
+  
+    logout() {
+      this.autenticaService.logout();
+      console.log('logout');
+    }
 }
